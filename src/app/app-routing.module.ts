8d522ee0a4f104dc53a32bea18f346665b01336f
Theo Freeman <theorfreeman@gmail.com>
2020-07-30 13:44:30 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { AviationComponent } from './components/aviation/aviation.component';
+import { ArtworkComponent } from './components/artwork/artwork.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    
+    component:HomeComponent
+  },
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"home"
+  },
+  {
+    path:"about",
+    component:AboutmeComponent
+  },
+  {
+    path:"aviation",
+    component:AviationComponent
+  },
+  {
+    path:"artwork",
+    component:ArtworkComponent
+  },
+  {
+    path:"**",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { AviationComponent } from './components/aviation/aviation.component';
+import { ArtworkComponent } from './components/artwork/artwork.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    AboutmeComponent,
+    AviationComponent,
+    ArtworkComponent
   ],
   imports: [
     BrowserModule,
