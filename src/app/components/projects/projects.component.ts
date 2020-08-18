8b67ec55ad72ef90959e33d3a0bb8eb0e0dd0217
Theo Freeman <theorfreeman@gmail.com>
2020-08-18 14:53:19 -0600

    created projects service. modified projects component to display projects from projects service.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/services/projects.service';
 
 @Component({
   selector: 'app-projects',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private ps:ProjectsService) { }
+  projects = this.ps.projects;
   ngOnInit(): void {
   }
 
---
+++ src/app/services/projects.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+  public projects= [
+    {
+      title: "DnD 5e Character Sheet",
+      date: "2020-07-24T00:00Z",
+      description: "A character sheet tool where users can create, edit, and download characters. \nThe character sheet does some extra useful things for players like:",
+      img: "/assets/images/CharSheetPreview.png",
+      bullets: [
+        "Compute Modifiers",
+        "Simple damage and healing buttons",
+        "A spellbook page where players can keep track of spells of all levels"
+      ],
+      gitURL: "https://github.com/TheoFree/Dnd5eCharacterSheet",
+      hostedURL: "https://dnd5e-character-sheet.web.app/home"
+    }
+    ,
+    {
+      title: "Api Explorer",
+      date: "2020-08-13T00:00Z",
+      description: "This evolved into a project as I was working on learning about APIs.\nAPIs implemented:",
+      img: "",
+      bullets: [
+        "Google Maps with a search bar that tries to autocomplete user entries",
+        "An interface for a local server and mongodb database that I made",
+        "Three different NASA APIs"
+      ],
+      gitURL: "",
+      hostedURL: ""
+    }
+  ]
+  constructor() {
+  }
+}
