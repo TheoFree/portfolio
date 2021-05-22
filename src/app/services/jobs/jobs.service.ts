31c81c0960e32f50ce3f75af82b2583ee416d2f0
Theo Freeman <theorfreeman@gmail.com>
2021-05-22 16:03:08 -0600

    working on layout, about me.

--- src/app/services/jobs/jobs.service.ts
+++ src/app/services/jobs/jobs.service.ts
@@ -6,6 +6,13 @@ import { Injectable } from '@angular/core';
 export class JobsService {
   jobs = [
     {
+      title:"IT Intern",
+      company:"The City of Boulder",
+      dates:"Jun 2014 - Dec 2014",
+      desc:`Assist city employees to solve technical difficulties. Manage computer software for various city departments and employees. Service
+      hardware around the city. Cycle out old hardware for new, set up new workstations. Destroy confidential information on computers to be recycled.`
+    },
+    {
       title:"Assembler 1",
       company:"Metalcraft Technologies Inc.",
       dates:"Oct 2020 - Present",
@@ -15,41 +22,13 @@ export class JobsService {
        clear of debris. `
     },
     {
-      title:"Line Service Technician",
-      company:"Journeys Aviation",
-      dates:"Feb 2020 - March 2020",
-      desc:`Act as customer service representative to customers inside FBO. Fuel, tow, clean aircraft. Dispatch aircraft
-      rentals. Terminated due to Covid-19.`
-    },
-    {
-      title:"Route Delivery Driver",
-      company:"Longmont Dairy Farm",
-      dates:"Oct 2019 - Dec 2019",
-      desc:`Deliver dairy products overnight to customers along pre-determined route. Responsible for loading correct product into truck,
-      driving to customer's homes safely and expediently, delivering all product to correct customers before 6:30AM, returning to unload
-      at the end of each night.`
-    },
-    {
       title:"Head Line Service Technician",
       company:"Fly Elite Aviation",
       dates:"May 2017 - Oct 2019",
       desc:`Manage fuel farm, train new employees, maintain fuel trucks, fuel, move, clean customer and flight school aircraft. Provide 
       customer service to all customers in FBO. Assist mechanics when able. Help management ensure compliance with fire and safety standards.`
-    },
-    {
-      title:"Delivery Driver",
-      company:"Jimmy Johns on the Hill",
-      dates:"May 2016 - Oct 2016",
-      desc:`Deliver sandwiches and other products to customers quickly and accurately. Maintain deli tidiness, provide excelent customer service
-      both in shop and to customers at delivery location. Help make sandwiches when able. `
-    },
-    {
-      title:"IT Intern",
-      company:"The City of Boulder",
-      dates:"Jun 2014 - Dec 2014",
-      desc:`Assist city employees to solve technical difficulties. Manage computer software for various city departments and employees. Service
-      hardware around the city. Cycle out old hardware for new, set up new workstations. Destroy confidential information on computers to be recycled.`
     }
+    
 
   ]
   getJobs=() => this.jobs;
