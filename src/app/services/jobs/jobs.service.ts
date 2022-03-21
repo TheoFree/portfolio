80fcb1be6d6588d1653abc8301b651bf63d63013
Theo Freeman <theorfreeman@gmail.com>
2022-03-20 18:14:47 -0600

    Updated Resume, Updated Projects, Updated About Me.

--- src/app/services/jobs/jobs.service.ts
+++ src/app/services/jobs/jobs.service.ts
@@ -6,27 +6,29 @@ import { Injectable } from '@angular/core';
 export class JobsService {
   jobs = [
     {
-      title:"IT Intern",
-      company:"The City of Boulder",
-      dates:"Jun 2014 - Dec 2014",
-      desc:`Assist city employees to solve technical difficulties. Manage computer software for various city departments and employees. Service
-      hardware around the city. Cycle out old hardware for new, set up new workstations. Destroy confidential information on computers to be recycled.`
-    },
-    {
-      title:"Assembler 1",
-      company:"Metalcraft Technologies Inc.",
+      title:"Assembler II",
+      company:"Align Precision - Cedar City",
       dates:"Oct 2020 - Present",
-      desc:`Complete assemblies from parts for customers in the aerospace industry. Commonly used tools: drills,
-       a variety of rivet squeezes, socket wrenches, countersinks, and so on. Responsible for completing jobs quickly
-       while maintaining high level of quality; staying within tight tolerances for error and keeping parts clean and 
-       clear of debris. `
+      desc:`Build complex military and commercial aircraft parts following blue prints and customer specifications.
+      Learn new tools and skills quickly to take on more challenging assemblies.
+      Create new methods for approaching difficult problems in complex assemblies.
+      Help mentor new Assemblers on a variety of jobs and skills.
+       `
     },
     {
       title:"Head Line Service Technician",
       company:"Fly Elite Aviation",
       dates:"May 2017 - Oct 2019",
       desc:`Manage fuel farm, train new employees, maintain fuel trucks, fuel, move, clean customer and flight school aircraft. Provide 
-      customer service to all customers in FBO. Assist mechanics when able. Help management ensure compliance with fire and safety standards.`
+      customer service to all customers in FBO. Assist mechanics when able. Help management ensure compliance with fire and safety standards.
+      Keep business website up to date with current fuel prices and services.`
+    },
+    {
+      title:"IT Intern",
+      company:"The City of Boulder IT Department",
+      dates:"Jun 2014 - Dec 2014",
+      desc:`Provide excellent customer service while responding to helpdesk tickets. Manage computer software for various city departments and employees. Service
+      hardware around the city. Exchange employee workstations following regular cycle. Destroy confidential information on computers to be recycled.`
     }
     
 
